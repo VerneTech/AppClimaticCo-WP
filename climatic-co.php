24bc2f865036e85859d514301942e904c960318e
Óscar Hernández Caballero <ohernandezc@vernegroup.com>
2022-11-29 16:49:21 +0100

    Fix ThankYoy Link , v1.0.7

--- climatic-co.php
+++ climatic-co.php
@@ -9,14 +9,14 @@
  * that starts the plugin.
  *
  * @link              https://www.climaticco.com/
- * @since             1.0.6
+ * @since             1.0.7
  * @package           climatic_co_Plugin
  *
  * @wordpress-plugin
  * Plugin Name:       ClimaticCo
  * Plugin URI:        https://www.climaticco.com/ayuda/wp-plugin-config/
  * Description:       La solución para la sostenibilidad de tu eCommerce: ClimaticCo hace que tus envíos sean neutros en carbono. Sencillamente.
- * Version:           1.0.6
+ * Version:           1.0.7
  * Update URI:        https://appv2.climaticco.com/wordpress-plugin/info.json
  * Author:            ClimaticCo
  * Author URI:        https://www.climaticco.com/
